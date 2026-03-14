c197e709331bb0b9cc7579a8355cf50258a426a2
djkorecos8 <ksrinrpi749@hotmail.com>
2026-03-14 13:14:58 +0800

    Add temperature conversion utilities

---
+++ src/utils/temperature.py
@@ -0,0 +1,7 @@
+def celsius_to_fahrenheit(c: float) -> float:
+    """Convert Celsius to Fahrenheit."""
+    return c * 9.0 / 5.0 + 32.0
+
+def fahrenheit_to_celsius(f: float) -> float:
+    """Convert Fahrenheit to Celsius."""
+    return (f - 32.0) * 5.0 / 9.0
